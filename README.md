cab64498f1925686cd13515c2722a7aa7a268481
Hannah Suarez hcs0 <hi@hannahsuarez.me>
2017-02-23 18:39:08 +0000

    Add Dynamic Text JS

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dynamic Changing Text Using Javascript
+
+<img src="https://d2r1vs3d9006ap.cloudfront.net/s3_images/1409479/327-vhko4_inline.gif?1462281010">
+<img src="https://d2r1vs3d9006ap.cloudfront.net/s3_images/1409480/30744-1381tk_inline.gif?1462281058">
---
+++ changingtext.js
@@ -0,0 +1,9 @@
+$(function() {
+	$("#typed").typed({
+		strings: ["Grace Padilla", "Cecil Drake", "Kate Owen", "Christy Simpson", "Joe Maldonado", "Raymond Nash", "Rita Mann", "Ray Howell", "Adrienne Barnes"],
+		typeSpeed: 100,
+    backDelay: 1500,
+    loop: true,
+    callback: function(){}
+	});
+});
